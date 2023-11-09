30c0ea64abee8b01544e19d07a4189056334a0b0
Shreyansh Kumbhare <124287359+Shreyansh784@users.noreply.github.com>
2023-11-09 08:20:19 +0530

    Add files via upload

---
+++ cryptocurrency-market-tracker/Currency Converter/api-key.js
@@ -0,0 +1,2 @@
+//Paste your generated api Key here
+let apiKey = "690d323e4348613597eb83a4";
---
+++ cryptocurrency-market-tracker/authentication-with-firebase/main.js
@@ -0,0 +1,44 @@
+// setting up firebase with our website
+const firebaseApp = firebase.initializeApp({
+    
+});
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+// Sign up function
+const signUp = () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    console.log(email, password)
+    // firebase code
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((result) => {
+            // Signed in 
+            document.write("You are Signed Up")
+            console.log(result)
+            // ...
+        })
+        .catch((error) => {
+            console.log(error.code);
+            console.log(error.message)
+            // ..
+        });
+}
+
+// Sign In function
+const signIn = () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    // firebase code
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((result) => {
+            // Signed in 
+            //document.write("You are Signed In")
+            window.location.href = "file:///D:/App/cryptocurrency-market-tracker/index.html"
+            console.log(result)
+        })
+        .catch((error) => {
+            console.log(error.code);
+            console.log(error.message)
+        });
+}
